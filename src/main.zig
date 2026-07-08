2a628a4168fc45cb603405f81d4a7caeeaa307fc
tacheraSasi <tacherasasi@gmail.com>
2026-07-08 14:42:52 +0300

    Walk docs directory instead of current directory

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const global = @import("global.zig");
 const Io = std.Io;
 
 const mf = @import("mf");
@@ -7,8 +8,16 @@ pub fn main(init: std.process.Init) !void {
     const allocator = init.gpa;
     const io = init.io;
     const cwd = std.Io.Dir.cwd();
-    const dir = cwd.openDir(io,".",.{ .iterate = true });
-    defer dir.close();
+    const dir = try cwd.openDir(io, "/Users/mac/work/akili/erp/backend/docs/", .{ .iterate = true });
+    defer dir.close(io);
 
-    
+    var walker = try dir.walk(allocator);
+    defer walker.deinit();
+
+    while (try walker.next(io)) |entry| {
+        if (std.mem.eql(u8, @tagName(entry.kind), "file")) {
+            continue;
+        }
+        std.debug.print("path: {s}, kind: {s}\n", .{ entry.path, @tagName(entry.kind) });
+    }
 }
