f5c8130eb8455b882fab2f91bde0a8de2e9bda1e
tacheraSasi <tacherasasi@gmail.com>
2026-07-14 14:51:54 +0300

    utils file for all the helpers

---
+++ src/utils.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+
+
+/// this function create a file only if it does not exist other wise it return the actual file itself
+pub fn createFileIfNotExist(io: std.Io, cwd: std.Io.Dir, sub_path: []const u8) !std.Io.File {
+    return cwd.createFile(std.Io, sub_path, .{
+        .read = true,
+        .exclusive = true,
+    }) catch |err| switch (err) {
+        error.PathAlreadyExists => {
+            return cwd.openFile(io, sub_path, .{
+                .mode = .read_only,
+            });
+        },
+        else => return err,
+    };
+}
