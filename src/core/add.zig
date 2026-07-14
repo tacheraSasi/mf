956c8c381c6c642eadd31cebafb14fac80651407
tacheraSasi <tacherasasi@gmail.com>
2026-07-14 14:51:25 +0300

    Implement git clone in add command and refactor scan function
    The `scan` function has been moved to its own module (`core/scan.zig`)
    and the `add` command now clones repositories using `git clone`.

--- src/core/add.zig
+++ src/core/add.zig
@@ -5,6 +5,14 @@ const cmd = @import("../cmd.zig");
 /// clones the repo then adds the dir and git url to the manifest
 /// mf add https://github/.....
 pub fn Add(io: std.Io, allocator: std.mem.Allocator, dir: std.Io.Dir, git_url: []const u8) !void{
-    
-    
+    // run git clone command
+    const argv = [_][]const u8{ "git", "clone", git_url };
+    _ = cmd.run(io, allocator, &argv) catch |err| switch (err) {
+        error.ExitCodeFailure => {
+            return std.debug.print("Failed to clone the repo",.{});
+        },
+        else => return err,
+    };
+
+    // after clone is done add the project to the manifest file
 }
