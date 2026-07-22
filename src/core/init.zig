2584aed81db723f601da1b14291856c42ac5e6a4
tacheraSasi <tacherasasi@gmail.com>
2026-07-22 13:13:16 +0300

    Add documentation comment to Init function

--- src/core/init.zig
+++ src/core/init.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const stdio = @import("stdio");
 
-
+/// Initializes the workspace by creating an empty manifest file `mf.manifest.json`
 pub fn Init(io: std.Io, dir: std.Io.Dir, sub_path: []const u8, console: *stdio.Console) !void {
     dir.createFile(io, sub_path, .{ .read = true, .exclusive = true }) catch |err| switch (err) {
         error.PathAlreadyExists => {
