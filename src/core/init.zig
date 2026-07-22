4c7e8c05e2a4a39ff26f287b901dde399f952a4b
tacheraSasi <tacherasasi@gmail.com>
2026-07-22 13:12:21 +0300

    Add workspace initialization function

---
+++ src/core/init.zig
@@ -0,0 +1,14 @@
+const std = @import("std");
+const stdio = @import("stdio");
+
+
+pub fn Init(io: std.Io, dir: std.Io.Dir, sub_path: []const u8, console: *stdio.Console) !void {
+    dir.createFile(io, sub_path, .{ .read = true, .exclusive = true }) catch |err| switch (err) {
+        error.PathAlreadyExists => {
+            try console.printLine("The mf workspace is already initialized", .{});
+            return;
+        },
+        else => return err,
+    };
+    try console.printLine("Workspace initialized successfully yeey.", .{});
+}
