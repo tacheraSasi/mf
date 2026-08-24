56b47ebb63bb3ef979a89bc779462318ae570be9
tacheraSasi <tacherasasi@gmail.com>
2026-08-24 10:39:46 +0300

    Pass io, allocator and dir to the Clone command

--- src/main.zig
+++ src/main.zig
@@ -57,7 +57,7 @@ pub fn main(init: std.process.Init) !void {
             try core.Add(io, allocator, dir, positional_args[0], &console);
         },
         .clone => {
-            try core.Clone(&console);
+            try core.Clone(io, allocator, dir, &console);
         },
         .status => try core.Status(io, allocator, dir, &console),
         .rm => {
