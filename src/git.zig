8c7e0d3d17a09eb63827d5cb1be59685bb0570a0
tacheraSasi <tacherasasi@gmail.com>
2026-08-24 12:04:37 +0300

    Stream git output to the console in GitClone

--- src/git.zig
+++ src/git.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+const stdio = @import("stdio");
+
 const cmd = @import("cmd.zig");
 
 // returns the git url of a project dir or an error
@@ -18,11 +20,15 @@ pub fn GetGitUrl(io: std.Io, allocator: std.mem.Allocator, entry_name: []const u
 }
 
 /// performs a git clone in the process cwd
-pub fn GitClone(io: std.Io, allocator: std.mem.Allocator, entry_name: []const u8) ![]const u8 {
+pub fn GitClone(io: std.Io, allocator: std.mem.Allocator, entry_name: []const u8, git_url: []const u8, console: *stdio.Console) !void {
     // git runs from process cwd, which IS the scanned dir (opened on ".").
     // So entry.name is a valid relative path for `git -C`.
-    const argv = [_][]const u8{ "git", "-C", entry_name, "remote", "get-url", "origin" };
-    const result = cmd.run(io, allocator, &argv) catch |err| switch (err) {
+    // const argv = [_][]const u8{ "git", "-C", entry_name, "clone", git_url };
+    _ = git_url;
+    const argv = [_][]const u8{ "echo", " yeeeey", "git", "-C", entry_name, "remote", "get-url", "origin" };
+    try console.printLine("cloning {s}...", .{entry_name});
+
+    const result = cmd.runStream(io, allocator, &argv, console.writer) catch |err| switch (err) {
         error.ExitCodeFailure => {
             return err;
         },
